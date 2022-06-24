495dcb34c8235eeb68aa7e4e27596ae069ac3e1e
BRUNO BARBOSA <brunobdq32@gmail.com>
2022-06-24 13:27:20 -0400

    buscar msg

--- Área de Trabalho/projeto5-batepapouol/script.js
+++ Área de Trabalho/projeto5-batepapouol/script.js
@@ -5,6 +5,7 @@ function buscarDados(){
     let promessa = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
     promessa.then(popularDados);
 }
+
  function popularDados(resposta){
     console.log(resposta);
     mensagens = resposta.data;  
@@ -12,51 +13,27 @@ function buscarDados(){
  }
 
  function renderizarReceitas(){
-     const ulMensagens = document.querySelector('.mensagens');
+     const ulMensagens1 = document.querySelector('.mensagens1');
+     ulMensagens1.innerHTML = "";
+     const ulMensagens = document.querySelector('.mensagens1');
      ulMensagens.innerHTML = "";
      
      for (let i = 0; i < mensagens.length; i++){
         if(mensagens[i].type === "status"){
-        ulMensagens.innerHTML += `
+        ulMensagens1.innerHTML += `
         <div class="caixa-1">
         <a>(${mensagens[i].time})</a><b>${mensagens[i].from}</b><p>${ mensagens[i].text}</p>
         </div>
         `;
      }
-     
+     else if(mensagens[i].type === "message"){
+      ulMensagens.innerHTML += `
+      <div class="caixa">
+      <a>(${mensagens[i].time})</a><b>${mensagens[i].from }</b>  para <b>${mensagens[i].to}</b> <p>${ mensagens[i].text}</p>
+      </div>
+      `;
+   }
     }
+   
+    
  }
-
-
-
-
-
-
-
-/*
-
-function renderizarMensagens(msg){
-    console.log(msg.data);
-    for (let i = 0; i < 100; i++){
-    var receitas = document.querySelector('.mensagens');
-    receitas.innerHTML += msg.data[msg.data.length-1].from;
-    console.log(receitas);
-    i++;
-    }
-}
-
-
-
-let elementoTempo = document.querySelector('.tempo');
-let elementoEmitente = document.querySelector('.emitente');
-let elementoPara = document.querySelector('.para');
-let elementoTexto = document.querySelector('.texto');
-
-
-elementoTempo.innerHTML = lista.data.time;
-elementoEmitente.innerHTML = lista.data.from;
-elementoPara.innerHTML = lista.data.to;
-elementoTexto.innerHTML = lista.data.text;
-
-
-*/
