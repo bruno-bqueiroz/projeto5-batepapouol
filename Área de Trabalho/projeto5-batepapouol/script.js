bfbf6abdce1244bcccda32f8098db6f7a7091a4d
BRUNO BARBOSA <brunobdq32@gmail.com>
2022-06-24 11:31:45 -0400

    status

--- Área de Trabalho/projeto5-batepapouol/script.js
+++ Área de Trabalho/projeto5-batepapouol/script.js
@@ -1,35 +1,51 @@
 let mensagens=[];
 
+buscarDados(); 
 function buscarDados(){
     let promessa = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
-    promessa.then(buscarDados);
+    promessa.then(popularDados);
 }
+ function popularDados(resposta){
+    console.log(resposta);
+    mensagens = resposta.data;  
+    renderizarReceitas();
+ }
 
-function buscarDados(resposta){
-    mensagens = resposta;
-    renderizarMensagens();
-}
+ function renderizarReceitas(){
+     const ulMensagens = document.querySelector('.mensagens');
+     ulMensagens.innerHTML = "";
+     
+     for (let i = 0; i < mensagens.length; i++){
+        if(mensagens[i].type === "status"){
+        ulMensagens.innerHTML += `
+        <div class="caixa-1">
+        <a>(${mensagens[i].time})</a><b>${mensagens[i].from}</b><p>${ mensagens[i].text}</p>
+        </div>
+        `;
+     }
+     
+    }
+ }
 
 
-function renderizarMensagens(){
-    const Receitas = document.querySelector('.mensagens');
 
-    for (let i = 0; i < mensagens.length; i++){
 
-        Receitas.innerHTML += `
-        <li>
-            ${mensagens}
-        </li>
-        `
-    }   
-}
-
-buscarDados();  
-renderizarMensagens();
 
 
 
 /*
+
+function renderizarMensagens(msg){
+    console.log(msg.data);
+    for (let i = 0; i < 100; i++){
+    var receitas = document.querySelector('.mensagens');
+    receitas.innerHTML += msg.data[msg.data.length-1].from;
+    console.log(receitas);
+    i++;
+    }
+}
+
+
 
 let elementoTempo = document.querySelector('.tempo');
 let elementoEmitente = document.querySelector('.emitente');
